e99cff6cccaec1f262228d4e569e163aa72fb5c6
seb <55889353+JustACasual@users.noreply.github.com>
2025-11-06 17:34:43 +0100

    change unimatrix to more general id dummy

--- scripts/step_5/pipelines/update_server.groovy
+++ scripts/step_5/pipelines/update_server.groovy
@@ -6,7 +6,7 @@
 // under default value → "+ Add" → "Jenkins"
 // Kind → Username and password
 // assign a username and password (the password has to be the same as for the linux user "jenkins") → "Add"
-// replace unimatrix_herdenadmin_pw from the script with the ID of your just created access data → "manage jenkins" → "Credentials"
+// replace your_credentials_id_here from the script with the ID of your just created access data → "manage jenkins" → "Credentials"
 //
 // Recommendation: Run once a night
 // H 4 * * *
@@ -17,21 +17,21 @@ pipeline {
     stages {
         stage('update') {
             steps {
-                withCredentials([usernamePassword(credentialsId: 'unimatrix_herdenadmin_pw', usernameVariable: 'SUDO_USER', passwordVariable: 'SUDO_PW')]) {
+                withCredentials([usernamePassword(credentialsId: 'your_credentials_id_here', usernameVariable: 'SUDO_USER', passwordVariable: 'SUDO_PW')]) {
                 sh 'echo $SUDO_PW | sudo -S apt update'
                 }
             }
         }
         stage('upgrade') {
             steps {
-                withCredentials([usernamePassword(credentialsId: 'unimatrix_herdenadmin_pw', usernameVariable: 'SUDO_USER', passwordVariable: 'SUDO_PW')]) {
+                withCredentials([usernamePassword(credentialsId: 'your_credentials_id_here', usernameVariable: 'SUDO_USER', passwordVariable: 'SUDO_PW')]) {
                 sh 'echo $SUDO_PW | sudo -S apt-get upgrade -y'
                 }
             }
         }
         stage('reboot') {
             steps {
-                withCredentials([usernamePassword(credentialsId: 'unimatrix_herdenadmin_pw', usernameVariable: 'SUDO_USER', passwordVariable: 'SUDO_PW')]) {
+                withCredentials([usernamePassword(credentialsId: 'your_credentials_id_here', usernameVariable: 'SUDO_USER', passwordVariable: 'SUDO_PW')]) {
                 sh 'echo $SUDO_PW | sudo -S shutdown -r 1'
                 }
             }
